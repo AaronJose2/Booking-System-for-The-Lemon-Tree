0c6b416def8d8cad7a030a716060f384c4a45854
AaronJose2 <8939@castellalunmail.com>
2022-12-31 17:19:21 +0000

    Updated

--- AdministratorScreen.py
+++ AdministratorScreen.py
@@ -1,9 +1,12 @@
 from tkinter import *
+from FontStyleSheet import *
 def openAdminWindow():
     #creates the administrative window
-    AdminWin = Toplevel()
-    AdminWin.geometry("400x400")
-    AdminWin.title("Administrative Menu")
+    adminWin = Toplevel()
+    adminWin.geometry("400x400")
+    adminWin.title("Administrative Menu")
     # main title
-    mainTitle = Label(mainWin, text="Login", font=Heading)
+    mainTitle = Label(adminWin, text="Administrative Window", font=Heading)
     mainTitle.pack()
+    #buttons and respective subroutines that lead to new windows
+    
