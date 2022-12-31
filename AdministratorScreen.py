ad71cf381f676f7a9d6a5a9d0947403a3e64c118
AaronJose2 <8939@castellalunmail.com>
2022-12-31 17:09:26 +0000

    updated

--- AdministratorScreen.py
+++ AdministratorScreen.py
@@ -1,5 +1,9 @@
 from tkinter import *
 def openAdminWindow():
+    #creates the administrative window
     AdminWin = Toplevel()
     AdminWin.geometry("400x400")
     AdminWin.title("Administrative Menu")
+    # main title
+    mainTitle = Label(mainWin, text="Login", font=Heading)
+    mainTitle.pack()
